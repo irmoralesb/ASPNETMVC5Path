a0b01ec719f3fdb5fa23e93a5a96322caa0202ee
Raul Morales <irmoralesb@hotmail.com>
2020-02-09 09:48:05 -0600

    Autofac - DI Setup

--- ImplementingAutofacInASPNet/EasyBlog/EasyBlog.Web/Global.asax.cs
+++ ImplementingAutofacInASPNet/EasyBlog/EasyBlog.Web/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using EasyBlog.Web.Core;
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using EasyBlog.Web.Core;
 using System;
 using System.Linq;
 using System.Web.Http;
@@ -22,6 +25,23 @@ namespace EasyBlog.Web
 
             if (Application["ModuleEvents"] == null)
                 Application["ModuleEvents"] = extensibilityManager.GetModuleEvents();
+
+            ContainerBuilder builder = new ContainerBuilder();
+            //No need to register each controller one by one
+            //builder.RegisterType<HomeController>();
+
+            //All controllers can be registered at once.
+            builder.RegisterControllers(typeof(MvcApplication).Assembly); //MVC
+            builder.RegisterApiControllers(typeof(MvcApplication).Assembly);//WebAPI
+            //Set the container
+            IContainer container = builder.Build();
+
+            //Dependency Resolver
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container)); //MVC
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);//WebAPI
+            
+
+
         }
     }
 }
